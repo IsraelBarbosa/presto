a47462b0ce550a9db7803ba3682395f9fc03c628
Israel Barbosa <medeirosisrael2000@gmail.com>
2020-04-08 13:03:47 -0300

    Adição do header e footer

--- buscar.php
+++
@@ -1,7 +0,0 @@
-<?php
-        echo "Desculpe, Nenhum profissional encontrado!<br>";
-        echo "Tente novamente mais tarde!<br>"; 
-        echo"<font color='green'>BUSCAR NOVAMENTE</font>";
-        echo"<br><a href='index.html'>Página inicial</a>";
-
-?>
